97b764d61471a5f91267bbff434dc0efe79c91a2
Sadiq <pageton@proton.me>
2026-05-31 01:50:30 +0300

    fix: add bun bin to PATH in zsh

--- home/zsh.nix
+++ home/zsh.nix
@@ -4,6 +4,10 @@
   programs.zsh = {
     enable = true;
     initContent = lib.mkBefore ''
+      # Bun
+      export BUN_INSTALL="$HOME/.bun"
+      export PATH="$BUN_INSTALL/bin:$PATH"
+
       # Ctrl+Arrow word navigation
       bindkey "^[[1;5C" forward-word
       bindkey "^[[1;5D" backward-word
