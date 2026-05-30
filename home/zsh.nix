b1e8b32bdc8863772800846ebbcc682f3fc3b202
Sadiq <pageton@proton.me>
2026-05-31 01:52:55 +0300

    fix: move bun PATH to zshenv so it's always available

--- home/zsh.nix
+++ home/zsh.nix
@@ -3,11 +3,12 @@
 {
   programs.zsh = {
     enable = true;
-    initContent = lib.mkBefore ''
+    envExtra = ''
       # Bun
       export BUN_INSTALL="$HOME/.bun"
       export PATH="$BUN_INSTALL/bin:$PATH"
-
+    '';
+    initContent = lib.mkBefore ''
       # Ctrl+Arrow word navigation
       bindkey "^[[1;5C" forward-word
       bindkey "^[[1;5D" backward-word
